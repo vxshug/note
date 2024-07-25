1a987e0c8dc137303486d595024fe67c32a1ed5b
vxshug <vxzyfx@gmail.com>
2024-07-25 18:22:30 +0800

    feat: linux

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 export default defineNuxtConfig({
-  ssr: true,
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
+
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible'
-  ]
+  ],
+  compatibilityDate: '2024-07-25',
 })
