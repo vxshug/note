dd7112cc64fdc0f4cf00678ddaa6f5debcbfaede
vxshug <vxzyfx@gmail.com>
2024-07-27 17:23:26 +0800

    feat: golang

--- app.config.ts
+++ app.config.ts
@@ -30,7 +30,7 @@ export default defineAppConfig({
     header: {
       logo: true,
       showLinkIcon: true,
-      exclude: ["/browser", "/project-start", "/database", "/cpp", "/qt", "/flutter", "/apple", "/note", "/linux"],
+      exclude: ["/browser", "/project-start", "/database", "/cpp", "/go", "/flutter", "/apple", "/note", "/linux"],
       fluid: true
     }
   }
