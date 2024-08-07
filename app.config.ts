29406d45882441f16b15f0fe65ee6dc2a15f7948
vxshug <vxzyfx@gmail.com>
2024-08-07 18:27:44 +0800

    feat: fs

--- app.config.ts
+++ app.config.ts
@@ -30,7 +30,7 @@ export default defineAppConfig({
     header: {
       logo: true,
       showLinkIcon: true,
-      exclude: ["/browser", "/project-start", "/database", "/cpp", "/go", "/flutter", "/apple", "/note", "/linux", "/protocol", "/rust", "/javascript", "/kj", "/language", "/python", "/asm", "/php", "/ruby", "/csharp"],
+      exclude: ["/browser", "/project-start", "/database", "/cpp", "/go", "/flutter", "/apple", "/note", "/linux", "/protocol", "/rust", "/javascript", "/kj", "/language", "/python", "/asm", "/php", "/ruby", "/csharp", "/fs"],
       fluid: true
     }
   }
